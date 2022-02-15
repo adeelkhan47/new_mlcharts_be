cc0cf31b12ae16a8b78e06cf045e4212e159fd1c
Shahroz Saleem <shahrozsaleem17@gmail.com>
2022-02-15 21:53:28 +0500

    Added /many routes

---
+++ src/utils/validation.util.js
@@ -0,0 +1,22 @@
+function isValidDataObj(obj) {
+  return !!(
+    obj &&
+    typeof obj == "object" &&
+    !(obj instanceof Array) &&
+    Object.keys(obj).length &&
+    obj.id &&
+    obj.label &&
+    obj.hasOwnProperty("value")
+  );
+}
+
+function isValidString(str) {
+  return !!(str && typeof str === "string" && str.trim().length);
+}
+
+const validationUtil = {
+  isValidDataObj,
+  isValidString
+};
+
+module.exports = validationUtil;
